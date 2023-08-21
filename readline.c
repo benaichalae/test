03da9607ba8d4774e7faf287f4187a665e5d171a
benaichalae <benaichalae@gmail.com>
2023-08-21 22:08:37 +0100

    shell

--- readline.c
+++ readline.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 char *read_line(void)
-{
+/**{
     char *line = NULL;
     size_t len = 0;
     ssize_t n;
@@ -15,4 +15,26 @@ char *read_line(void)
         return (NULL);
     }
     return (line);
+}*/
+{
+    char *line = NULL;
+    size_t bufsize = 0;
+    ssize_t n;
+
+    // Check if stdin is connected to a terminal
+    if (isatty(STDIN_FILENO)) {
+        write(STDOUT_FILENO, "$ ", 2);
+        fflush(stdout);  // Flush the output to ensure prompt is displayed
+    }
+
+    // Read a line of text
+    n = getline(&line, &bufsize, stdin);
+
+    // Check for errors or end of input
+    if (n == -1 || (n == 0 && line[0] == '\n')) {
+        free(line);
+        return NULL;
+    }
+
+    return line;
 }

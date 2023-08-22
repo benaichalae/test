10bf899f1967735c803301fee3485ce8a92c4065
Alae ben aicha <75539159+benaichalae@users.noreply.github.com>
2023-08-22 10:05:15 +0100

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,24 +1,30 @@
 #include "shell.h"
 
+/**
+ * _execute - execute command in parent process then child process
+ * @cmd: pointer to a pointer to command array
+ * @argv: pointer to a pointer to the argument vector
+ * Reutrun: exit status of a child process
+ */
 int _execute(char **cmd, char **argv)
 {
-    pid_t child;
-    int status;
+        pid_t child;
+        int status;
 
-    child = fork();
-    if (child == 0)
-    {
-        if (execve(cmd[0], cmd, environ) == -1)
+        child = fork();
+        if (child == 0)
         {
-            perror(argv[0]);
-            free_array(cmd);
-            exit(127);
+                if (execve(cmd[0], cmd, environ) == -1)
+                {
+                        perror(argv[0]);
+                        free_array(cmd);
+                        exit(127);
+                }
         }
-    }
-    else
-    {
-        waitpid(child, &status, 0);
-        free_array(cmd);
-    }
-    return (WEXITSTATUS(status));
+        else
+        {
+                waitpid(child, &status, 0);
+                free_array(cmd);
+        }
+        return (WEXITSTATUS(status));
 }
